3f5bf0741fde85a9a3007e8da15b102ba5987029
Buzz <Buzz627@gmail.com>
2019-04-17 10:05:53 -0400

    added normilazation and more functions

--- entropy.py
+++ entropy.py
@@ -2,6 +2,37 @@ import pymongo
 import json
 import math
 
+def normilize(data, fields=[]):
+	normilizedData=map(lambda x: {"rating":x["rating"]}, data)
+	if fields==[]:
+		keys=data[0].keys()
+	else:
+		keys=fields
+
+	for k in keys:
+		try:
+			avg=average(map(lambda x: x[k], data))
+			sig=sigma(map(lambda x: x[k], data))
+			for i in range(len(data)):
+				normilizedData[i][k]=(data[i][k]-avg)/float(sig)
+
+			
+		except KeyError:
+			print ">>>>>>> key error: "+ k
+		
+	return normilizedData
+
+def average(lst):
+	try:
+		return math.fsum(lst)/len(lst)
+	except: 
+		return 0 
+def sigma(lst):
+	avg=average(lst)
+	squares=map(lambda x: math.pow(x-avg, 2), lst) 
+	meanSquares=math.fsum(squares)/len(lst)
+	return math.sqrt(meanSquares)
+
 
 def makeBuckets(data, field, num):
 
@@ -54,60 +85,89 @@ def informationGain(data, entropyField, classification):
 
 	return entropyBefore - entropyAfter
 
+def getTop(data, fields, n):
+	infoGain=[]
+	for field in fields:
+		infoGain.append((field, informationGain(data, field, "rating")))
 
+	infoGain.sort(key = lambda x: x[1])
+	return infoGain[0-n:]
 
-client=pymongo.MongoClient()
-db = client.weather
-collection = db.conditions
-
-entropyField="cloudCover"
-
-data=[]
-results=collection.find({})
-for d in results:
-	# print d.keys()
-	data.append(d)
-
-# print data[0]
-
-fields=["cloudCover", 
-	"temperature", 
-	"dewPoint", 
-	# "nearestStormBearing", 
-	"windBearing", 
-	"nearestStormDistance", 
-	"visibility", 
-	"apparentTemperature", 
-	"pressure", 
-	"humidity", 
-	"ozone",
-	"windSpeed", 
-	"windGust", 
-	"precipIntensity", 
-	"uvIndex", 
-	"precipProbability"]
-
-# buckets=makeBuckets(data, entropyField, 4)
+def getData():
+	client=pymongo.MongoClient()
+	db = client.weather
+	collection = db.conditions
+	data=[]
+	results=collection.find({})
+	for d in results:
+		# print d.keys()
+		data.append(d)
+	return data
 
 
 
 
-infoGain=[]
 
-for field in fields:
-	infoGain.append((field, informationGain(data, field, "rating")))
+if __name__=="__main__":
+	entropyField="cloudCover"
 
-infoGain.sort(key = lambda x: x[1])
-for i in infoGain:
-	print i
+	data=getData()
 
-# data=[{"rating":"m", "ends-vowel":0}]*6
-# data.extend([{"rating":"m", "ends-vowel":1}]*3)
-# data.extend([{"rating":"f", "ends-vowel":0}]*1)
-# data.extend([{"rating":"f", "ends-vowel":1}]*4)
-# entropyField="ends-vowel"
+	# print data[0]
 
-# print informationGain(data, entropyField, "rating")
+	fields=["cloudCover", 
+		"temperature", 
+		"dewPoint", 
+		# "nearestStormBearing", 
+		"windBearing", 
+		"nearestStormDistance", 
+		"visibility", 
+		"apparentTemperature", 
+		"pressure", 
+		"humidity", 
+		"ozone",
+		"windSpeed", 
+		"windGust", 
+		"precipIntensity", 
+		"uvIndex", 
+		"precipProbability"]
+
+	# buckets=makeBuckets(data, entropyField, 4)
+
+
+	infoGain=[]
+
+	for field in fields:
+		infoGain.append((field, informationGain(data, field, "rating")))
+
+	infoGain.sort(key = lambda x: x[1])
+	for i in infoGain:
+		print i
+	# print getTop(data,fields, 2)
+	normalData=normilize(data, fields)
+	print "\n\nnormal"
+	infoGain=[]
+	for field in fields:
+		infoGain.append((field, informationGain(normalData, field, "rating")))
+
+	infoGain.sort(key = lambda x: x[1])
+	for i in infoGain:
+		print i
+
+
+
+
+
+	########## TESTING
+	# print sigma([9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4])
+
+	# data=[{"rating":"m", "ends-vowel":0}]*6
+	# data.extend([{"rating":"m", "ends-vowel":1}]*3)
+	# data.extend([{"rating":"f", "ends-vowel":0}]*1)
+	# data.extend([{"rating":"f", "ends-vowel":1}]*4)
+	# entropyField="ends-vowel"
+
+	# print informationGain(data, entropyField, "rating")
 
 
 
