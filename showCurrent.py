95b3ec50dd0d005b430e19bd4c7693cb0d5957ad
Buzz <Buzz627@gmail.com>
2019-04-18 11:59:16 -0400

    show current

---
+++ showCurrent.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+url = 'http://ipinfo.io/json'
+ipInfo=requests.get("https://ipinfo.io").json()
+
+apiKey=os.environ["APIKEY"]
+lat=ipInfo["loc"].split(",")[0].strip()
+lon=ipInfo["loc"].split(",")[1].strip()
+print ipInfo["city"], lat, lon
+r = requests.get('https://api.darksky.net/forecast/{}/{},{}?exclude=hourly,minutely,daily'.format(apiKey, lat, lon))
+result=r.json()
+print json.dumps(result['currently'], indent=4)
