b452ab8c14342c29b0080a8a47e4b85285133a34
Buzz <Buzz627@gmail.com>
2019-06-04 14:37:39 -0400

    added apis and heroku build stuff

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory, jsonify, request
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def test():
+	return send_from_directory(".", "location.html")
+
+@app.route("/locationTest", methods=['POST'])
+def locTest():
+	data = json.loads(request.data)
+	print(data)
+	print(data['latitude'])
+	return ""
